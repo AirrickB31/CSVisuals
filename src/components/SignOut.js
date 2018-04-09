34deb7c92dd944fbab0e7768d919deea5ea89b4b
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-04-09 14:36:56 -0700

    Add routes and pages, add SignUp Page Component

---
+++ src/components/SignOut.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const SignOutPage = () =>
+<div>
+<h1>SignOut Page</h1>
+</div>
+export default SignOutPage;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCQG1sEbCVYYmCxBlhbY0ACZsP25BHh0u0",
+    authDomain: "privet-83a48.firebaseapp.com",
+    databaseURL: "https://privet-83a48.firebaseio.com",
+    projectId: "privet-83a48",
+    storageBucket: "privet-83a48.appspot.com",
+    messagingSenderId: "864513816118"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+
+export {
+    auth,
+};
