b176b4cd49bd15e45c1011c6c9c03a0ce0956bbb
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-04-12 13:31:37 -0700

    Add paswword change form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
-import { SignUpLink, SignUpForm } from './SignUp';
+import { SignUpLink } from './SignUp';
+import { PasswordForgetLink } from './PasswordForget';
 import { auth } from '../firebase';
 import * as routes from '../constants/routes';
 
@@ -9,6 +10,7 @@ const SignInPage = ({ history }) =>
   <div>
     <h1>SignIn</h1>
     <SignInForm history={history} />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 
