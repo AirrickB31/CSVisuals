bb171b9769968195de07fd919e30a900a72f6334
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-04-13 14:11:06 -0700

    compulsory commit

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,94 +1,82 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
 
-import { SignUpLink } from './SignUp';
-import { PasswordForgetLink } from './PasswordForget';
-import { auth } from '../firebase';
+import {SignUpLink} from './SignUp';
+import {PasswordForgetLink} from './PasswordForget';
+import {auth, firebase} from '../firebase';
 import * as routes from '../constants/routes';
 
-const SignInPage = ({ history }) =>
-  <div>
-    <h1>SignIn</h1>
-    <SignInForm history={history} />
-    <PasswordForgetLink />
-    <SignUpLink />
-  </div>
+const SignInPage = ({history}) => <div>
+  <h1>SignIn</h1>
+  <SignInForm history={history}/>
+  <PasswordForgetLink/>
+  <SignUpLink/>
+</div>
 
-const byPropKey = (propertyName, value) => () => ({
-  [propertyName]: value,
-});
+const byPropKey = (propertyName, value) => () => ({[propertyName]: value});
 
 const INITIAL_STATE = {
   email: '',
   password: '',
-  error: null,
+  user: null,
+  error: null
 };
 
 class SignInForm extends Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = { ...INITIAL_STATE};
-    }
-    onSubmit = (event) => {
-      const {
-        email,
-        password,
-      } = this.state;
+    this.state = {
+      ...INITIAL_STATE
+    };
+  }
 
-      const {
-        history,
-      } = this.props;
+  onSubmit = (event) => {
+    const {email, password} = this.state;
 
-      auth.doSignInWithEmailAndPassword(email, password)
-        .then(() => {
-          this.setState(() => ({ ...INITIAL_STATE}));
-          history.push(routes.HOME);
-        })
-        .catch(error => {
-          this.setState(byPropKey('error', error));
-        });
+    const {history} = this.props;
 
-      event.preventDefault();
-    }
+    auth
+      .doSignInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState(() => ({
+          ...INITIAL_STATE
+        }));
+        history.push(routes.HOME);
+      })
+      .catch(error => {
+        this.setState(byPropKey('error', error));
+      });
 
-    render(){
-        const {
-          email,
-          password,
-          error,
-        } = this.state;
+    event.preventDefault();
+  }
 
-        const isInvalid =
-          password === '' ||
-          email === '';
-        
-        return (
-            <form onSubmit={this.onSubmit}>
-              <input
-                type="text"
-                value={email}
-                onChange={event => this.setState(byPropKey('email', event.target.value ))}
-                placeholder='Email Address'
-              />
-              <input
-                type="password"
-                value={password}
-                onChange={event => this.setState(byPropKey('password', event.target.value ))}
-                placeholder='Password'
-              />
-              <button disabled={isInvalid} type="submit">
-                Sign In
-              </button>
-              { error && <p>{error.massage}</p>}
-            </form>
-        );
-    }
+  render() {
+    const {email, password, user, error} = this.state;
+
+    const isInvalid = password === '' || email === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
+          placeholder='Email Address'/>
+        <input
+          type="password"
+          value={password}
+          onChange={event => this.setState(byPropKey('password', event.target.value))}
+          placeholder='Password'/>
+        <button disabled={isInvalid} type="submit">
+          Sign In
+        </button>
+        {error && <p>{error.massage}</p>}
+      </form>
+    );
+  }
 }
-
 
 export default withRouter(SignInPage);
 
-export {
-    SignInForm,
-};
+export {SignInForm};
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -18,5 +18,5 @@ const auth = firebase.auth();
 
 export {
     db,
-    auth,
+    auth
 };
